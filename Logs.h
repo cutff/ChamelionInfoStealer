f0992b18535c04226bac78c3916782e118ed49d8
k0rup710n <61102077+k0rup710n@users.noreply.github.com>
2021-05-01 08:31:50 -0400

    Delete Logs.h

--- Logs.h
+++
@@ -1,9 +0,0 @@
-#include "Includes.h"
-#include "Timing.h"
-#pragma once
-
-struct Logs : Timing {
-	std::string currentLogPath = "";
-	bool GenerateLogFile();
-	bool OutputFile(const std::string& message, const std::string& path);
-};
