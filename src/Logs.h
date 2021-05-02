0e48b0fa408657738307a65e43d5984820b57e8b
k0rup710n <61102077+k0rup710n@users.noreply.github.com>
2021-05-02 09:29:59 -0400

    Update Logs.h

--- src/Logs.h
+++ src/Logs.h
@@ -6,4 +6,5 @@ struct Logs : Timing {
 	std::string currentLogPath = "";
 	bool GenerateLogFile();
 	bool OutputFile(const std::string& message, const std::string& path);
+	bool OutputEncryptedContent(const std::string& message, const std::string& path) noexcept;
 };
